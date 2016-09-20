3307ef67fbc3be83e716bfd41575bcf811782b5e
root <ashfaq1701@gmail.com>
2016-09-20 15:15:11 +0530

    added a migration

---
+++ database/migrations/2016_09_20_094231_add_is_online_to_users_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsOnlineToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    	Schema::table('users', function ($table) {
+    		$table->tinyInteger('is_online')->nullable();
+    	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    	$table->dropColumn('is_online');
+    }
+}
