84ac5160247509cfc6340352d35b2687300c09e6
root <ashfaq1701@gmail.com>
2016-09-22 16:16:43 +0530

    simple chatting ui

--- resources/views/chats/index.blade.php
+++ resources/views/chats/index.blade.php
@@ -5,22 +5,37 @@
 	<div class="row">
 		<div class="col-md-2">
 			<div class="row">
-				<div class="col-md-12">
+				<a class="col-md-12 chat-grid">
 					One
-				</div>
-				<div class="col-md-12">
+				</a>
+				<a class="col-md-12 chat-grid">
 					Two
-				</div>
-				<div class="col-md-12">
+				</a>
+				<a class="col-md-12 chat-grid">
 					Three
-				</div>
+				</a>
 			</div>
 		</div>
 		<div class="col-md-10">
 			<div class="panel panel-default" id="chat-panel">
   				<div class="panel-heading" id="chat-heading">Panel Heading</div>
   				<div class="panel-body" id="chat-body">Panel Content</div>
-  				<div class="panel-footer" id="chat-footer">Panel Footer</div>
+  				<div class="panel-footer" id="chat-footer">
+  					<div class="row">
+  						<div class="col-md-1">
+							<label class="btn btn-primary">
+    							File
+    							<input type="file" id="chat-file-input" style="display: none;">
+							</label>
+						</div>
+  						<div class="col-md-10">
+  							<input type="text" class="form-control" id="message-text" placeholder="Write message here..."/>
+  						</div>
+  						<div class="col-md-1">
+  							<button class="btn btn-default">Send</button>
+  						</div>
+  					</div>
+				</div>
 			</div>
 		</div>
 	</div>
